402ac9bdfad8a064c1eef76652f017988afbdf75
Luca Wimmer <info@lucawimmer.de>
2014-07-28 23:46:20 +0200

    Added teleport-height

--- src/main/java/de/lucawimmer/lobbytools/commands/LobbyToolsCommand.java
+++ src/main/java/de/lucawimmer/lobbytools/commands/LobbyToolsCommand.java
@@ -19,11 +19,11 @@ public class LobbyToolsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = null;
-        if(!(sender instanceof ConsoleCommandSender)) p = (Player) sender;
+        if (!(sender instanceof ConsoleCommandSender)) p = (Player) sender;
 
         if (args.length == 1 && args[0].equalsIgnoreCase("reload") && sender.hasPermission("lobbytools.admin.reload")) {
             config.reloadConfig();
-            if(sender instanceof ConsoleCommandSender)
+            if (sender instanceof ConsoleCommandSender)
                 Bukkit.getLogger().info("The configuration file for LobbyTools was reloaded successfully.");
             else
                 sender.sendMessage("§c[§6LobbyTools§c] §fThe configuration file was reloaded successfully.");
@@ -47,14 +47,17 @@ public class LobbyToolsCommand implements CommandExecutor {
             config.saveLocation("lobbytools.exact-spawn-loc", ((Player) sender).getLocation());
             sender.sendMessage("§c[§6LobbyTools§c] §fYou have set the exact spawn-point.");
             config.saveConfig();
+        } else if (args.length == 1 && (args[0].equalsIgnoreCase("settploc") || args[0].equalsIgnoreCase("settl") || args[0].equalsIgnoreCase("setteleport")) && sender.hasPermission("lobbytools.admin.teleportloc") && !(sender instanceof ConsoleCommandSender)) {
+            config.saveLocation("lobbytools.teleport-height-location", ((Player) sender).getLocation());
+            sender.sendMessage("§c[§6LobbyTools§c] §fYou have set the teleport-height location.");
+            config.saveConfig();
         } else {
-            if((sender instanceof ConsoleCommandSender))
+            if ((sender instanceof ConsoleCommandSender))
                 Bukkit.getLogger().info("This command is player only.");
+            else if (!sender.hasPermission("lobbytools.admin.setspawn") || !sender.hasPermission("lobbytools.admin.globalmute"))
+                sender.sendMessage("§c[§6LobbyTools§c] §fYou don't have the permissions to do that!");
             else
-                if(!sender.hasPermission("lobbytools.admin.setspawn") || !sender.hasPermission("lobbytools.admin.globalmute"))
-                    sender.sendMessage("§c[§6LobbyTools§c] §fYou don't have the permissions to do that!");
-                else
-                    sender.sendMessage("§c[§6LobbyTools§c] §fWrong syntax! Use /lobbytools <reload/toggle/globalmute/setpsawn>");
+                sender.sendMessage("§c[§6LobbyTools§c] §fWrong syntax! Use /lobbytools <reload/toggle/globalmute/setpsawn>");
         }
         return true;
     }
